a3817287b8287d19603c972be931f095bc5c326f
Zes <sergey_the_great@mail.ru>
2019-01-22 20:40:47 +0500

    Сделал задание 1 урока.

--- app/src/main/java/zesley/sergey/homework2/MainActivity.java
+++ app/src/main/java/zesley/sergey/homework2/MainActivity.java
@@ -2,12 +2,65 @@ package zesley.sergey.homework2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.subjects.PublishSubject;
 
 public class MainActivity extends AppCompatActivity {
+    EditText mEditTextTask1;
+    TextView mTextViewTask1;
+    PublishSubject<String> subTask1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEditTextTask1=findViewById(R.id.edit_text_task1);
+        mTextViewTask1=findViewById(R.id.text_view_task1);
+        subTask1=PublishSubject.create();
+        subTask1.subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                mTextViewTask1.setText(s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+
+        mEditTextTask1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                subTask1.onNext(mEditTextTask1.getText().toString());
+            }
+        });
+
     }
 }
