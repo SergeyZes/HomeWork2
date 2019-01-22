f0d13de64af267b760d5f24759226a2d112d128d
Zes <sergey_the_great@mail.ru>
2019-01-22 21:00:03 +0500

    Сделал интерфейс для задания 2.

--- app/src/main/java/zesley/sergey/homework2/MainActivity.java
+++ app/src/main/java/zesley/sergey/homework2/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,14 +16,21 @@ public class MainActivity extends AppCompatActivity {
     EditText mEditTextTask1;
     TextView mTextViewTask1;
     PublishSubject<String> subTask1;
+    EditText mEditTextSrc1;
+    EditText mEditTextSrc2;
+    EditText mEditTextEventBus;
+    Button mButtonSrc1;
+    Button mButtonSrc2;
+    Button mButtonEventBus;
+    TextView mTextViewSubscriber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mEditTextTask1=findViewById(R.id.edit_text_task1);
-        mTextViewTask1=findViewById(R.id.text_view_task1);
-        subTask1=PublishSubject.create();
+        mEditTextTask1 = findViewById(R.id.edit_text_task1);
+        mTextViewTask1 = findViewById(R.id.text_view_task1);
+        subTask1 = PublishSubject.create();
         subTask1.subscribe(new Observer<String>() {
             @Override
             public void onSubscribe(Disposable d) {
@@ -61,6 +69,15 @@ public class MainActivity extends AppCompatActivity {
                 subTask1.onNext(mEditTextTask1.getText().toString());
             }
         });
+
+        mEditTextSrc1 = findViewById(R.id.edit_text_task2_src1);
+        mEditTextSrc2 = findViewById(R.id.edit_text_task2_src2);
+        mEditTextEventBus = findViewById(R.id.edit_text_event_bus);
+
+        mButtonSrc1 = findViewById(R.id.btn_task2_src1);
+        mButtonSrc2 = findViewById(R.id.btn_task2_src2);
+        mButtonEventBus = findViewById(R.id.btn_event_bus);
+        mTextViewSubscriber = findViewById(R.id.text_view_subscriber);
 
     }
 }
