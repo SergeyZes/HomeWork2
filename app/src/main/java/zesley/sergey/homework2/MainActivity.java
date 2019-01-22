ebda0f90616761f9c20a405fe27a48fba10570e2
Zes <sergey_the_great@mail.ru>
2019-01-22 22:35:04 +0500

    Закончил 1-е и 2-е задание.

--- app/src/main/java/zesley/sergey/homework2/MainActivity.java
+++ app/src/main/java/zesley/sergey/homework2/MainActivity.java
@@ -4,10 +4,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import io.reactivex.Observable;
 import io.reactivex.Observer;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.subjects.PublishSubject;
@@ -23,6 +25,10 @@ public class MainActivity extends AppCompatActivity {
     Button mButtonSrc2;
     Button mButtonEventBus;
     TextView mTextViewSubscriber;
+    TextView mTextViewSubscriber2;
+    PublishSubject<String> subSrc1;
+    PublishSubject<String> subSrc2;
+    EventBus eventBus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,6 +76,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
         mEditTextSrc1 = findViewById(R.id.edit_text_task2_src1);
         mEditTextSrc2 = findViewById(R.id.edit_text_task2_src2);
         mEditTextEventBus = findViewById(R.id.edit_text_event_bus);
@@ -78,6 +85,74 @@ public class MainActivity extends AppCompatActivity {
         mButtonSrc2 = findViewById(R.id.btn_task2_src2);
         mButtonEventBus = findViewById(R.id.btn_event_bus);
         mTextViewSubscriber = findViewById(R.id.text_view_subscriber);
+        mTextViewSubscriber2 = findViewById(R.id.text_view_subscriber2);
+        subSrc1 = PublishSubject.create();
+        subSrc2 = PublishSubject.create();
+        eventBus = new EventBus(subSrc1, subSrc2);
+
+        mButtonSrc1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                subSrc1.onNext(mEditTextSrc1.getText().toString());
+            }
+        });
+        mButtonSrc2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                subSrc2.onNext(mEditTextSrc2.getText().toString());
+            }
+        });
+        mButtonEventBus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                eventBus.onNext(mEditTextEventBus.getText().toString());
+            }
+        });
+
+        eventBus.subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                mTextViewSubscriber.setText(s);
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+        eventBus.subscribe(new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(String s) {
+                mTextViewSubscriber2.setText(s);
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
 
     }
 }
